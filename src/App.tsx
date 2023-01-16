b00ccc2dd8ac671e56a87f77bcb0d830fd676ada
suqun zhang <susizhang2017@gmail.com>
2023-01-16 10:07:55 +0100

    adding Navbar and shopping cart icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { BsFillCartFill } from "react-icons/bs";
+export const Navbar = () => {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <BsFillCartFill />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%,25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+};
